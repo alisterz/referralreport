2b066cd7e66b2efa5fec4496678f482009467268
Xing Chen Zhao <xingchen.zhao@ca.com>
2019-03-24 22:48:53 -0700

    format code

--- src/main/java/ReferralReportApplication.java
+++ src/main/java/ReferralReportApplication.java
@@ -5,32 +5,32 @@ import com.streak.referral.ReferralReport;
 import java.util.Arrays;
 
 public class ReferralReportApplication {
-  private static Gson gson = new Gson();
-  private static String REFERRAL_IMPORT =
-      "[\n"
-          + "      {\n"
-          + "        \"name\": \"Acme, Inc.\", \"close_date\": \"01-12-2018\", \"referred_by\": \"Sprockets\"\n"
-          + "      },\n"
-          + "      {\n"
-          + "        \"name\": \"Sprockets\", \"close_date\": \"01-09-2018\", \"referred_by\": \"Dunder Mifflin\"\n"
-          + "      },\n"
-          + "      {\n"
-          + "        \"name\": \"Initech\", \"close_date\": \"01-01-2018\"\n"
-          + "      },\n"
-          + "      {\n"
-          + "        \"name\": \"Initrode\", \"close_date\": \"02-02-2018\", \"referred_by\": \"Initech\"\n"
-          + "      },\n"
-          + "      {\n"
-          + "        \"name\": \"L&P Construction\", \"close_date\": \"01-19-2018\", \"referred_by\": \"Initech\"\n"
-          + "      },\n"
-          + "      {\n"
-          + "        \"name\": \"Dunder Mifflin\", \"close_date\": \"12-01-2017\"\n"
-          + "      }\n"
-          + "    ]";
+    private static Gson gson = new Gson();
+    private static String REFERRAL_IMPORT =
+            "[\n"
+                    + "      {\n"
+                    + "        \"name\": \"Acme, Inc.\", \"close_date\": \"01-12-2018\", \"referred_by\": \"Sprockets\"\n"
+                    + "      },\n"
+                    + "      {\n"
+                    + "        \"name\": \"Sprockets\", \"close_date\": \"01-09-2018\", \"referred_by\": \"Dunder Mifflin\"\n"
+                    + "      },\n"
+                    + "      {\n"
+                    + "        \"name\": \"Initech\", \"close_date\": \"01-01-2018\"\n"
+                    + "      },\n"
+                    + "      {\n"
+                    + "        \"name\": \"Initrode\", \"close_date\": \"02-02-2018\", \"referred_by\": \"Initech\"\n"
+                    + "      },\n"
+                    + "      {\n"
+                    + "        \"name\": \"L&P Construction\", \"close_date\": \"01-19-2018\", \"referred_by\": \"Initech\"\n"
+                    + "      },\n"
+                    + "      {\n"
+                    + "        \"name\": \"Dunder Mifflin\", \"close_date\": \"12-01-2017\"\n"
+                    + "      }\n"
+                    + "    ]";
 
-  public static void main(String[] args) {
-    Referral[] referrals = gson.fromJson(REFERRAL_IMPORT, Referral[].class);
-    ReferralReport rp = new ReferralReport();
-    rp.printReport(Arrays.asList(referrals));
-  }
+    public static void main(String[] args) {
+        Referral[] referrals = gson.fromJson(REFERRAL_IMPORT, Referral[].class);
+        ReferralReport rp = new ReferralReport();
+        rp.printReport(Arrays.asList(referrals));
+    }
 }
--- src/main/java/com/streak/referral/Referral.java
+++ src/main/java/com/streak/referral/Referral.java
@@ -5,39 +5,39 @@ import com.google.gson.annotations.SerializedName;
 
 public class Referral {
 
-  @SerializedName("name")
-  @Expose
-  private String name;
+    @SerializedName("name")
+    @Expose
+    private String name;
 
-  @SerializedName("close_date")
-  @Expose
-  private String closeDate;
+    @SerializedName("close_date")
+    @Expose
+    private String closeDate;
 
-  @SerializedName("referred_by")
-  @Expose
-  private String referredBy;
+    @SerializedName("referred_by")
+    @Expose
+    private String referredBy;
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public String getCloseDate() {
-    return closeDate;
-  }
+    public String getCloseDate() {
+        return closeDate;
+    }
 
-  public void setCloseDate(String closeDate) {
-    this.closeDate = closeDate;
-  }
+    public void setCloseDate(String closeDate) {
+        this.closeDate = closeDate;
+    }
 
-  public String getReferredBy() {
-    return referredBy;
-  }
+    public String getReferredBy() {
+        return referredBy;
+    }
 
-  public void setReferredBy(String referredBy) {
-    this.referredBy = referredBy;
-  }
+    public void setReferredBy(String referredBy) {
+        this.referredBy = referredBy;
+    }
 }
